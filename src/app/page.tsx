d5f3d95d8b55f9c5522da4cdf979062c7b183b91
Matheus <matheusdamatag@gmail.com>
2024-01-04 23:07:06 -0300

    fix: enabled admin validation again for page redirection after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,8 @@ import { useEffect } from 'react'
 export default function Home() {
   const router = useRouter()
   useEffect(() => {
-    // ALERT - disabled until back-end works again
-    // const user = JSON.parse(localStorage.getItem('user') || '')
-    const user = {
-      type: 'user',
-    }
+    const user = JSON.parse(localStorage.getItem('user') || '')
+
     if (user?.type === 'admin') {
       router.push('/admin/user')
     } else {
